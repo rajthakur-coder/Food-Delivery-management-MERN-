ce31128f86ab7039c85e0e7baf60f0cae212aa2b
Raj Thakur <raj8827778809@gmail.com>
2025-02-10 13:54:53 +0530

    Update orderController.js

--- Backhand/controllers/orderController.js
+++ Backhand/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:3000"
+    const frontend_url = "https://food-delivery-management-mern.onrender.com"
 
     try {
         const newOrder = new orderModel({
