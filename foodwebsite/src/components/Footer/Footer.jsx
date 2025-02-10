c6f4bb5a3dc703c07189cf6caa1af55762900905
Raj <raj8827778809@gmail.com>
2025-02-10 13:41:15 +0530

    Fixed issue

--- foodwebsite/src/components/Footer/Footer.jsx
+++ foodwebsite/src/components/Footer/Footer.jsx
@@ -27,7 +27,7 @@ function Footer() {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-564-6754</li>
+                <li>+91 8827788763</li>
                 <li>contact@tomato.com</li>
             </ul>
         </div>
